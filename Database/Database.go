402815272fc6f3737fb221952d774252f41e66a2
ThoLorenz <Asinwgnb;dDmeed.iu17>
2021-08-06 15:30:18 +0200

    Service f. Funksteckdose (nicht fertig)

--- Database/Database.go
+++ Database/Database.go
@@ -2,32 +2,65 @@ package database
 
 import (
 	models "GartenBewaesserung/Models"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+var db *gorm.DB
+
+type Connection struct {
+	Provider   string
+	ConnString string
+}
+
 func InitDatabase() {
-	db, err := gorm.Open("mysql",
-		"snooker147admin:admin2020lorenz@tcp(127.0.0.1:3306)/gartenbewaesserung?charset=utf8&parseTime=True")
+	conn := GetConnectionString()
+	db, err := gorm.Open(conn.Provider, conn.ConnString)
 	db.SingularTable(true)
-	//defer db.Close()
+	defer db.Close()
 	if err != nil {
 		log.Println("Database Connection konnte nicht hergestellt werden")
 	} else {
 		log.Println("Database Connection erfolgreich erstellt")
-
-		log.Println("... erstelle Tabelle 'Funksteckdose")
-		db.Debug().AutoMigrate(&models.Funksteckdose{})
-
-		log.Println("... erstelle Tabelle 'Wasserventil")
-		db.Debug().AutoMigrate(&models.Wasserventil{})
-
-		log.Println("... erstelle Tabelle 'Feuchtigkeitssensor")
-		db.Debug().AutoMigrate(&models.Feuchtigkeitssensor{})
-
-		log.Println("... erstelle Tabelle 'Hochbeet")
-		db.Debug().AutoMigrate(&models.Hochbeet{})
+		AutoMigrateDB()
 	}
+}
+
+func GetConnectionString() *Connection {
+	// read file
+	data, err := ioutil.ReadFile("./Config/DatabaseConnection.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+	var conn Connection
+	err = json.Unmarshal(data, &conn)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return &conn
+}
+
+func AutoMigrateDB() {
+	conn := GetConnectionString()
+	db, err := gorm.Open(conn.Provider, conn.ConnString)
+	defer db.Close()
+	if err != nil {
+		fmt.Print(err)
+	}
+	log.Println("... erstelle Tabelle 'Funksteckdose")
+	db.Debug().AutoMigrate(&models.Funksteckdose{})
+
+	log.Println("... erstelle Tabelle 'Wasserventil")
+	db.Debug().AutoMigrate(&models.Wasserventil{})
+
+	log.Println("... erstelle Tabelle 'Feuchtigkeitssensor")
+	db.Debug().AutoMigrate(&models.Feuchtigkeitssensor{})
+
+	log.Println("... erstelle Tabelle 'Hochbeet")
+	db.Debug().AutoMigrate(&models.Hochbeet{})
 }
