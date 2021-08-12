d62c086809350480df594c00879edeed10bda9b8
ThoLorenz <Asinwgnb;dDmeed.iu17>
2021-08-12 14:01:33 +0200

    Löschen von Steckdosen

--- Services/FunksteckdosenService.go
+++ Services/FunksteckdosenService.go
@@ -2,45 +2,66 @@ package Services
 
 import (
 	database "GartenBewaesserung/Database"
+	helper "GartenBewaesserung/Helper"
 	models "GartenBewaesserung/Models"
 	"encoding/json"
-	_ "strconv"
+	"strconv"
 	"time"
-	_ "time"
 
 	//	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	_ "github.com/gorilla/mux"
+	mux "github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// Funksteckdose anlegen
 func Create(w http.ResponseWriter, r *http.Request) {
-
 	conn := database.GetConnectionString()
 	db, err := gorm.Open(conn.Provider, conn.ConnString)
 	if err != nil {
 		log.Println("Database Connection konnte nicht hergestellt werden")
+		return
 	} else {
-		fmt.Println("-- erstelle Funksteckdose --")
-	}
-
+	defer db.Close()
 	model := models.Funksteckdose{}
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&model); err != nil {
-		fmt.Println(http.StatusBadRequest, err.Error())
+		helper.ResponseError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	model.ErstelltAm = time.Now()
 	defer r.Body.Close()
 	if err := db.Save(&model).Error; err != nil {
-		fmt.Println(w, http.StatusInternalServerError, err.Error())
+		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 	fmt.Println(" ******* Steckdose wurde erstellt ")
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	conn := database.GetConnectionString()
+	db, err := gorm.Open(conn.Provider, conn.ConnString)
+	if err != nil {
+		log.Println("Database Connection konnte nicht hergestellt werden")
+		return
+	} else {
+		defer db.Close()
+		vars := mux.Vars(r)
+		id, _ := strconv.Atoi(vars["id"])
+		steckdose := GetFunksteckdoseOr404(db, id, w, r)
+		if steckdose == nil {
+			return
+		}
+		if err := db.Delete(&steckdose).Error; err != nil {
+			helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		helper.ResponseJSON(w, http.StatusOK, nil)
+	}
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
@@ -49,8 +70,8 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Database Connection konnte nicht hergestellt werden")
 	} else {
+		defer db.Close()
 		fmt.Println("-- hole Funksteckdosen --")
-
 		funksteckdosen := []models.Funksteckdose{}
 		db.Find(&funksteckdosen)
 		fmt.Println(&funksteckdosen)
@@ -58,8 +79,6 @@ func Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetList(w http.ResponseWriter, r *http.Request) {
-
-	// GetList from DB
 	conn := database.GetConnectionString()
 	db, err := gorm.Open(conn.Provider, conn.ConnString)
 	listFunksteckdose := []models.Funksteckdose{}
@@ -67,16 +86,22 @@ func GetList(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Database Connection konnte nicht hergestellt werden")
 	} else {
-		fmt.Println("-- Daten holen (Funksteckdose) --")
-
+		fmt.Println("-- List Funksteckdose --")
 		db.Find(&listFunksteckdose)
-
 		for index, element := range listFunksteckdose {
-			fmt.Println("nr: ", index, "Name: ", element.Kennung+" "+element.Systemcode)
+			fmt.Println("nr: ", index, "Name: ", element.Name+" "+element.Systemcode)
 			// index is the index where we are
 			// element is the element from someSlice for where we are
 		}
 		//	json.NewEncoder(w).Encode(listFunksteckdose)
 	}
 
+}
+func GetFunksteckdoseOr404(db *gorm.DB, id int, w http.ResponseWriter, r *http.Request) *models.Funksteckdose {
+	funksteckdose := models.Funksteckdose{}
+	if err := db.First(&funksteckdose, id).Error; err != nil {
+		helper.ResponseError(w, http.StatusNotFound, err.Error())
+		return nil
+	}
+	return &funksteckdose
 }
