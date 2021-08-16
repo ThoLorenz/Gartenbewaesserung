76f7c46d4a5f83b58e80584f75a3e37130f47910
ThoLorenz <Asinwgnb;dDmeed.iu17>
2021-08-16 16:12:23 +0200

    DeleteSensorById (Hochbeet)

--- Models/Hochbeet.go
+++ Models/Hochbeet.go
@@ -1,17 +1,43 @@
 package Models
 
 import (
-	_ "fmt"
 	"time"
 )
 
 type Hochbeet struct {
-	ID           int                   `gorm:"column:id;autoIncrement;type:int"`
-	Name         string                `sql:"size:30" gorm:"column:name;not null"`
-	ListSensoren []Feuchtigkeitssensor // `gorm:"foreignKey:ID"`
-	ErstelltAm   time.Time             `gorm:"column:erstelltAm"`
-	GeändertAm   time.Time             `gorm:"column:geaendertAm"`
-	GelöschtAm   time.Time             `gorm:"column:geloeschtAm"`
+	ID           int    `gorm:"column:id;autoIncrement;type:int"`
+	Name         string `sql:"size:30" gorm:"column:name;not null"`
+	ListSensoren []Feuchtigkeitssensor
+	ErstelltAm   time.Time `gorm:"column:erstelltAm"`
+	GeändertAm   time.Time `gorm:"column:geaendertAm"`
+	GelöschtAm   time.Time `gorm:"column:geloeschtAm"`
+}
+
+func Create(name string) *Hochbeet {
+	return &Hochbeet{Name: name, ErstelltAm: time.Now()}
+}
+
+// func AddSingleSensor(beet Hochbeet, sensor Feuchtigkeitssensor) *Hochbeet {
+// 	beet.ListSensoren = append(beet.ListSensoren, sensor)
+// 	return &beet
+// }
+
+// https://stackoverflow.com/a/18203895
+func SliceIndex(limit int, predicate func(i int) bool) int {
+	for i := 0; i < limit; i++ {
+		if predicate(i) {
+			return i
+		}
+	}
+	return -1
+}
+
+func DeleteSingleSensorById(beet Hochbeet, SensorId int) {
+	index := SliceIndex(len(beet.ListSensoren), func(i int) bool { return beet.ListSensoren[i].ID == SensorId })
+	// Remove the element at index i from a.
+	beet.ListSensoren[index] = beet.ListSensoren[len(beet.ListSensoren)-1] // Copy last element to index i.
+	beet.ListSensoren[len(beet.ListSensoren)-1] = Feuchtigkeitssensor{}    // Erase last element (write zero value).
+	beet.ListSensoren = beet.ListSensoren[:len(beet.ListSensoren)-1]       // Truncate slice.
 }
 
 // func GeneriereHochbeet(vent *Wasserventil) *Hochbeet {
